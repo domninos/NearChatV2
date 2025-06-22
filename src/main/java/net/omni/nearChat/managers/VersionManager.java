a7d74ecca7241b73b09b00d52eb7960068024bcf
domninos <68453370+domninos@users.noreply.github.com>
2025-06-22 19:35:01 +0800

    Implementing fetching updates on GitHub Releases.
    Test more (Spark)
    
    To-do:
      - add blocked/censored/blacklisted words
      - add admin command to inspect nearchat of players.
      - fetch for updates on load.
      - implement missing databases. (mongodb, mysql)
      - fix spacing when logging on paper 1.11.2 and lower on sending list messages
      - Redo messages (make sure to replace on MessageHandler.java
      - Put all messages on messages.yml.
      - **** CLEANUP ****
    
    To follow: GUIHandler.java
    
    Reviewed-by: domninos <68453370+domninos@users.noreply.github.com>

---
+++ src/main/java/net/omni/nearChat/managers/VersionManager.java
@@ -0,0 +1,10 @@
+package net.omni.nearChat.managers;
+
+public class VersionManager {
+    private static final String API_URL = "";
+
+    // TODO
+
+
+    // TODO GitHub Release to check if there is an update
+}
